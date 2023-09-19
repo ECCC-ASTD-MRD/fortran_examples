6370304510f2e0db8925c6251f8dc87c8417970b
M. Valin <mfvalin@gmail.com>
2023-09-19 12:11:44 -0400

    run a test with multiple compilers

---
+++ testall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for compiler in gnu/13 intel/23.2  aocc/4.1.0 nvhpc/23.7 # llvm/17
+do
+ module unload gnu intel aocc nvhpc llvm 2>&1 >/dev/null
+ module load $compiler 2>&1 >/dev/null
+ echo "======================= $compiler ======================="
+ $FCOMP "$@" && ./a.out
+ rm -f ./a.out
+done
