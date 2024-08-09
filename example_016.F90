679507331eef5207dd4f00520cbef4342be17f9c
M. Valin <mfvalin@gmail.com>
2024-08-09 10:18:12 -0400

    adding flesh to example

--- example_016.F90
+++ example_016.F90
@@ -47,19 +47,21 @@ contains
       deallocate(this%item_1d)
       print *,'pointer component item_1d deallocated'
     else
-      print *,'no need to deallocate pointer component item_1d'
+      print *,'no need to deallocate unassociated pointer component item_1d'
     endif
     if(associated(this%item_2d)) then
       deallocate(this%item_2d)
       print *,'pointer component item_2d deallocated'
     else
-      print *,'no need to deallocate pointer component item_2d'
+      print *,'no need to deallocate unassociated pointer component item_2d'
     endif
   end subroutine
 end module
 
 program example_016
   implicit none
+  call sub0
+  print *,'=================================='
   call sub1
   print *,'=================================='
   call sub2
@@ -89,6 +91,38 @@ subroutine sub_block
   item1%item_1d = [1]   ! implicit allocation for item1%item_1d (allocatable, not pointer item)
   print *,'exiting sub_block'
 end subroutine ! item1 and item3 from subroutine now "out of scope"
+
+subroutine sub0
+  use module_016
+  implicit none
+  type(elastic_pointer) :: item3
+  integer, dimension(:), pointer   :: p1
+  integer, dimension(:,:), pointer :: p2
+
+  print *,'entering sub0'
+  if(associated(item3%item_1d)) then
+    print *,'item3%item_1d is associated with size =',size(item3%item_1d)
+  else
+    print *,'item3%item_1d is not yet associated'
+  endif
+  print *,'in sub0'
+  allocate( p1(5) )
+  nullify(p2)
+  item3 = elastic_pointer(p1, p2)         ! creator
+  if(associated(item3%item_1d)) then
+    print *,'item3%item_1d is associated with size =',size(item3%item_1d)
+  else
+    print *,'item3%item_1d is not yet associated'
+  endif
+  if(associated(item3%item_2d)) then
+    print *,'item3%item_2d is associated with size =',size(item3%item_2d)
+  else
+    print *,'item3%item_2d is not associated'
+  endif
+
+  print *,'exiting sub0'
+! when this subroutine returns, item3 will become "out of scope"
+end subroutine
 
 subroutine sub1
   use module_016
