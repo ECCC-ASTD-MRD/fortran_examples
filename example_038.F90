190213fc520694f96f84f3c4d038a3e4ea4aac0a
M. Valin <mfvalin@gmail.com>
2025-05-27 09:59:55 -0400

    internal pseudo random function, revised test

--- example_038.F90
+++ example_038.F90
@@ -1,46 +1,57 @@
 ! there are sums ... and there is SUM()
 ! -O2 used by default
 ! intel/25.1  (seems to have a better intrinsic in library for -O2 and to recognize sum in explicit code)
-! sum/(ni*nj) =  0.50001  , z4/(ni*nj)  =  0.49999  , z8/(ni*nj)  =  0.50001  , z0/(ni*nj)  =  0.50001  (-O2)
-! sum/(ni*nj) =  0.25000  , z4/(ni*nj)  =  0.25000  , z8/(ni*nj)  =  0.50001  , z0/(ni*nj)  =  0.50001  (-O0) (-O1)
+!  (deviation from .5)   , sum = .352E-05  , z4  = .894E-07  , z8  = .596E-07  , z0  = .596E-07 (-O2)
+!  (deviation from .5)   , sum = .250E+00  , z4  = .250E+00  , z8  = .596E-07  , z0  = .596E-07 (-O0) (-O1)
 ! gnu/14
-! sum/(ni*nj) =  0.25000  , z4/(ni*nj)  =  0.25000  , z8/(ni*nj)  =  0.50001  , z0/(ni*nj)  =  0.50001
+!  (deviation from .5)   , sum = .250E+00  , z4  = .250E+00  , z8  = .596E-07  , z0  = .596E-07
 ! aoccc/5.0.0
-! sum/(ni*nj) =  0.25000  , z4/(ni*nj)  =  0.25000  , z8/(ni*nj)  =  0.50000  , z0/(ni*nj)  =  0.50000
+!  (deviation from .5)   , sum = .250E+00  , z4  = .250E+00  , z8  = .596E-07  , z0  = .596E-07
 ! Nvidia/PGI/25.5
-! sum/(ni*nj) =  0.25000  , z4/(ni*nj)  =  0.25000  , z8/(ni*nj)  =  0.50000  , z0/(ni*nj)  =  0.50000
+!  (deviation from .5)   , sum = .119E-06  , z4  = .119E-06  , z8  = .596E-07  , z0  = .596E-07
 ! llvm flang/20
-! sum/(ni*nj) =  0.25000  , z4/(ni*nj)  =  0.25000  , z8/(ni*nj)  =  0.49995  , z0/(ni*nj)  =  0.49995
+!  (deviation from .5)   , sum = .596E-07  , z4  = .250E+00  , z8  = .596E-07  , z0  = .596E-07
 !
 program example_038
   use ISO_C_BINDING
   implicit none
   integer, parameter :: NI = 8192, NJ = 8192
-  interface getrss
+  interface
     function getrss() result(amount) bind(C,name='get_rss')
       import C_INT64_T
       integer(C_INT64_T) :: amount
+    end function
+    function my_rand_f() result(r) bind(C,name='my_rand_f')
+      import C_FLOAT
+      real(C_FLOAT) :: r
     end function
   end interface
   real, dimension(NI,NJ) :: z
   real(kind=4) :: z8, z4, z0, zs
   integer, allocatable :: seed(:)
-  integer :: n
+  integer :: n, i, j
   real(kind=4), external :: sum4, sum0
   real(kind=8), external :: sum8
-!   call random_init(.false.,.false.)
-!   call random_seed(size = n)
-!   allocate(seed(n))
-!   call random_seed(get=seed)
-!   write (*, *) n, seed
-  call random_number(z)
-  zs = sum(z)
-  z8 = sum8(z,NI*NJ)
-  z4 = sum4(z,NI*NJ)
-  z0 = sum0(z,NI*NJ)
-  print 1, 'example_038, rss =', getrss(),', sum/(ni*nj) = ', zs/(ni*nj),', z4/(ni*nj)  = ', z4/(ni*nj),&
-           ', z8/(ni*nj)  = ', z8/(ni*nj),', z0/(ni*nj)  = ', z0/(ni*nj)
-1 format (A,I12,4(2X,A,F8.5))
+  integer, dimension(0:10) :: dist
+
+  dist = 0
+  do j=1,nj
+  do i=1,ni
+    z(i,j) = my_rand_f()
+    n = 10*z(i,j)
+    dist(n) = dist(n)+1
+  enddo
+  enddo
+  if(sum(dist(0:9)) .ne. ni*nj) stop
+  print '(A,11I8)', 'dist =',dist
+  zs = ABS(sum(z)/(ni*nj)-.5)
+  z8 = ABS(sum8(z,NI*NJ)/(ni*nj)-.5)
+  z4 = ABS(sum4(z,NI*NJ)/(ni*nj)-.5)
+  z0 = ABS(sum0(z,NI*NJ)/(ni*nj)-.5)
+  print 1, 'example_038 (deviation from .5) ',&
+           ', sum = ', zs,', z4  = ', z4,&
+           ', z8  = ', z8,', z0  = ', z0
+1 format (A,4(2X,A,E8.3))
 end program
 ! DUMB sum
 real(kind=4) function sum4(f, n)
--- utilities.c
+++ utilities.c
@@ -2,8 +2,23 @@
 #include <sys/resource.h>
 #include <stdint.h>
 
+static uint32_t my_seed = 0xBEBEFADA ;
+
 int64_t get_rss(){
   struct rusage rsrc ;
   int status = getrusage(RUSAGE_SELF, &rsrc) ;
   return rsrc.ru_maxrss ;
+}
+
+uint32_t my_rand_i(){
+  my_seed = 1664525 * my_seed + 1013904223 ;
+  return my_seed ;
+}
+
+float my_rand_f(){
+  union { uint32_t u ; float f ; } uf ;
+  uf.u = my_rand_i() ;
+  uf.u &= 0x7FFFFF ;        // keep lower 23 bits
+  uf.u |= (127 << 23) ;
+  return (uf.f - 1.0f) ;
 }
