61580ddbf3c498e4f6aa642bb949592d4824b248
M. Valin <mfvalin@gmail.com>
2026-02-25 14:30:03 -0500

    cosmetic change

--- example_043.F90
+++ example_043.F90
@@ -1,44 +1,42 @@
 ! expected output
-!  --------------------------  array   -----------------------
-! 1  'line 1                          '
-! 2  'line 2                          '
-! 3  'line 3                          '
-! 4  'line 4                          '
-! 5  'line 5                          '
-! 6  '                                '
-! 7  'empty                           '
-! 8  'empty                           '
-!  --------------------------  as is   -----------------------
-! 'line 1
-! line 2
-! line 3
-! line 4
-! line 5                              '
-!  --------------------------   trim   -----------------------
-! 'line 1
-! line 2
-! line 3
-! line 4
-! line 5'
-!  -------------------------- adjustr  -----------------------
-! '                              line 1
-! line 2
-! line 3
-! line 4
-! line 5'
-!
-! with some compilers : (/ after last item written is ignored)
-!  --------------------------  array   -----------------------
-! 1  'line 1                          '
-! 2  'line 2                          '
-! 3  'line 3                          '
-! 4  'line 4                          '
-! 5  'line 5                          '
-! 6  'empty                           '
-! 7  'empty                           '
-! 8  'empty                           '
-
-
+#if 0
+length of string =  75
+string = 'A,S,S,S,S,S,S,S,S,U,V,U,V,S,S,U,V,S,S,U,V,S,S,S,S,U,V,S,U,V,S,S,S,S,S,S,S,Z'
+SUCCESS
+--------------------------  array   -----------------------
+1  'line 1                          '
+2  'line 2                          '
+3  'line 3                          '
+4  'line 4                          '
+5  'line 5                          '
+6  'empty                           '
+7  'empty                           '
+8  'empty                           '
+--------------------------  as is   -----------------------
+'line 1
+line 2
+line 3
+line 4
+line 5                              '
+--------------------------   trim   -----------------------
+'line 1
+line 2
+line 3
+line 4
+line 5'
+-------------------------- adjustr  -----------------------
+'                              line 1
+line 2
+line 3
+line 4
+line 5'
+-------------------------- adjustl  -----------------------
+'line 1
+line 2
+line 3
+line 4
+line 5
+#endif
 program test_strings
   implicit none
   integer, PARAMETER :: cplatm_n_fldou=38
@@ -54,7 +52,7 @@ program test_strings
                       'S'  ,'S'  ,'S'  ,'S'  ,        &
                       'S'  ,'S'  ,'S'  ,'Z'  /
 
-  character(len=:), allocatable :: string
+  character(len=:), allocatable    :: string
   character(len=64)                :: long_string
   character(len=32), dimension(8)  :: array_string
   integer :: i
@@ -75,8 +73,7 @@ program test_strings
   endif
 
 ! multi line write into a string
-  long_string = ' '
-  array_string = 'empty'
+  array_string = 'empty'         ! initialize all elements to 'empty'
   write(array_string,2) 'line 1', 'line 2', 'line 3', 'line 4', 'line 5'
   print '(A)', '--------------------------  array   -----------------------'
   do i=1,size(array_string)
@@ -91,9 +88,11 @@ program test_strings
   print '(A)', "'" // trim(long_string) // "'"
   print '(A)', '-------------------------- adjustr  -----------------------'
   print '(A)', "'" // adjustr(long_string) // "'"
+  print '(A)', '-------------------------- adjustl  -----------------------'
+  print '(A)', "'" // adjustl(long_string) // "'"
 
 1 format("string = '",A,"'")
-2 format(8(A,/))
+2 format(8(A,:,/))
 3 format(I1,2X,A)
 end
 
